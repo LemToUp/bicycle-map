17f069c18816a2af3af2b83648ef348b59adfcf9
Dmitry Rumyantsev <LemToUp@users.noreply.github.com>
2019-07-16 13:58:02 +0300

    Robots txt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const app = express();
+const https = require('https');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 
@@ -11,28 +12,37 @@ const key = params.development.key;
 const port = params.development.port;
 const bot = new TelegramBot(key, {polling: true});
 
+const data = [];
+
 app.use(bodyParser.json());
 
 // Endpoints
 app.post(`/${key}`, (req, res) => {
-    console.log(req.body);
-    res.status(200).send(req.body);
+    data.push(req.body);
+    res.status(200).send([...data]);
 });
 
 // Endpoints
 app.get('/', (req, res) => {
-    console.log(req.body);
     res.send(req.body);
 });
 
+// Endpoints
+app.get('/robots.txt', (req, res) => {
+    fs.readFile('./robots.txt', 'utf8', function(err, contents) {
+        res.status(200).send(contents);
+    });
+});
+
 // Listening
-app.listen(port, () => {
+https.createServer({
+    key: fs.readFileSync('./server.key'),
+    cert: fs.readFileSync('./server.cert')
+}, app).listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
-console.log(key);
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
+/*bot.onText(/\/echo (.+)/, (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
@@ -42,4 +52,4 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
     // send back the matched "whatever" to the chat
     bot.sendMessage(chatId, resp);
-});
+});*/
